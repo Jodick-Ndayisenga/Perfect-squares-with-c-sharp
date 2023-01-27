9a9073ec41ab3df3213e11be176c5c290703c372
Jodick Ndayisenga <rajajodick@gmail.com>
2023-01-27 16:27:16 +0300

    'Square'

---
+++ square.cs
@@ -0,0 +1,14 @@
+using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(" Number\tSquare\tCube");
+        Console.WriteLine($" {0}\t{0 * 0}\t{0 * 0 * 0}");
+        Console.WriteLine($" {1}\t{1 * 1}\t{1 * 1 * 1}");
+        Console.WriteLine($" {2}\t{2 * 2}\t{2 * 2 * 2}");
+        Console.WriteLine($" {3}\t{3 * 3}\t{3 * 3 * 3}");
+        Console.WriteLine($" {4}\t{4 * 4}\t{4 * 4 * 4}");
+        Console.WriteLine($" {5}\t{5 * 5}\t{5 * 5 * 5}");
+    }
+}
